0c3fde7fa56a424b49db61f93345b68be3efaaf6
Alex Cone <alexcone@live.com>
2016-10-18 18:02:15 -0500

    Fixes/additions to currently walking screens and beginning google map api implementation

---
+++ app/src/main/java/ee461l/surewalk/CurrentlyWalkingRequesterScreen.java
@@ -0,0 +1,10 @@
+package ee461l.surewalk;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by alexc on 10/18/2016.
+ */
+
+public class CurrentlyWalkingRequesterScreen extends AppCompatActivity {
+}
---
+++ app/src/main/java/ee461l/surewalk/CurrentlyWalkingWalkerScreen.java
@@ -0,0 +1,8 @@
+package ee461l.surewalk;
+
+/**
+ * Created by alexc on 10/18/2016.
+ */
+
+public class CurrentlyWalkingWalkerScreen {
+}
