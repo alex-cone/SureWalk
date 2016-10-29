eb4afab536877f2ba330d64c33d76b1b0fe80a0b
DiegoGuerra1994 <dguerra95@utexas.edu>
2016-10-29 16:43:02 -0500

    Added volley for database usage Added basic Login/Register pages

--- app/src/main/java/ee461l/surewalk/HomeScreen.java
+++ app/src/main/java/ee461l/surewalk/HomeScreen.java
@@ -21,49 +21,9 @@ public class HomeScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ArrayList<SureWalkProfile> users = new ArrayList<SureWalkProfile>();
-                users.add(new Walker());
-                users.add(new Requester());
-
-                for(SureWalkProfile user:users){
-                    user.createTestUser();
-                    Log.d("SureWalke", user.getEmail());
-                }
-            }
-        });
+        setContentView(R.layout.login);
 
 
 
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home_screen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
