f81e721a3cfd7eef78b61f8bdc1735cb28ec4511
DiegoGuerra1994 <dguerra95@utexas.edu>
2016-10-30 15:52:02 -0500

    Adding communication to server

---
+++ app/src/main/java/app/AppConfig.java
@@ -0,0 +1,12 @@
+package app;
+
+/**
+ * Created by Diego on 10/30/2016.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.116.1/sure_walk/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.116.1/sure_walk/register.php";
+}
