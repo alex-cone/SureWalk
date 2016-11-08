0155451c3002936cf9c30f28a4374e8f2ce4138c
Alex Cone <alexcone@live.com>
2016-11-08 17:39:10 -0600

    no message

--- app/src/main/java/ee461l/surewalk/LookAtRequests.java
+++ app/src/main/java/ee461l/surewalk/LookAtRequests.java
@@ -1,29 +1,78 @@
 package ee461l.surewalk;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+import org.w3c.dom.Text;
 
 public class LookAtRequests extends AppCompatActivity {
-
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference mDatabase;
+    private ImageView profilePic;
+    private MapView requesterLoc;
+    private TextView profileName;
+    private TextView requesterAddress;
+    private Button btnAccept;
+    private Button btnDecline;
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_look_at_requests);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
+        profilePic = (ImageView) findViewById(R.id.imageView2);
+        //profilePic.setImageResource();
+        profileName = (TextView) findViewById(R.id.RequesterName);
+        profileName.setText("yolo");
+        requesterAddress = (TextView) findViewById(R.id.DestinationAddress);
+        requesterAddress.setText("yolo");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //Accept Request Button
+        btnAccept = (Button) findViewById(R.id.AcceptRequestButton);
+        btnAccept.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
             }
         });
+
+        //Decline Request Button
+        btnDecline = (Button) findViewById(R.id.DeclineRequestButton);
+        btnDecline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
