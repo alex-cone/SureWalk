7fd03e10d9f6fdb72e4407381a15ffccb70ff310
Jiwhan Son <jiwhanese@gmail.com>
2016-11-24 17:31:49 -0600

    Changed marker colors to differentiate current location & destination location. Added marker for walkers current location to AcceptRequestScreen.

--- app/src/main/java/ee461l/surewalk/AcceptRequestScreen.java
+++ app/src/main/java/ee461l/surewalk/AcceptRequestScreen.java
@@ -2,7 +2,12 @@ package ee461l.surewalk;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
@@ -12,10 +17,16 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
@@ -35,7 +46,14 @@ import Users.Walker;
 /**
  * Created by Diego on 11/7/2016.
  */
-public class AcceptRequestScreen extends FragmentActivity implements OnMapReadyCallback {
+public class AcceptRequestScreen extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener
+{
+    public static final String TAG = AcceptRequestScreen.class.getSimpleName();
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
     private ImageView profilePicture;
     private TextView txtName;
     private TextView txtPhoneNumber;
@@ -43,6 +61,9 @@ public class AcceptRequestScreen extends FragmentActivity implements OnMapReadyC
     private Button btnBackToRequests;
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Location walkerLocation;
 
     private String requestKey;
     private Request currentRequest;
@@ -51,9 +72,20 @@ public class AcceptRequestScreen extends FragmentActivity implements OnMapReadyC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.walker_accept_request_screen);
 
+        //Map and Location Services
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.walkerMap);
         mapFragment.getMapAsync(this);
+        //Initialize Google API Client
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
 
         Bundle extras = getIntent().getExtras();
         if(extras != null){
@@ -87,7 +119,7 @@ public class AcceptRequestScreen extends FragmentActivity implements OnMapReadyC
                         LatLng destinationLoc = new LatLng(currentRequest.getDestinationLatitude(), currentRequest.getDestinationLongitude());
 
                         MarkerOptions requesterMarker = new MarkerOptions().position(requesterLoc);
-                        MarkerOptions destinationMarker = new MarkerOptions().position(destinationLoc);
+                        MarkerOptions destinationMarker = new MarkerOptions().position(destinationLoc).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
 
                         mMap.addMarker(requesterMarker);
                         mMap.addMarker(destinationMarker);
@@ -133,5 +165,86 @@ public class AcceptRequestScreen extends FragmentActivity implements OnMapReadyC
 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "Location services connected.");
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location == null) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+        else {
+            handleNewLocation(location);
+        }
+    }
+
+    private void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+        walkerLocation = location; // Set the current location data to the current location
+
+
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title("Current Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+        mMap.addMarker(options);
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        //mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Location services suspended. Please reconnect.");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //setUpMapIfNeeded();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
     }
 }
--- app/src/main/java/ee461l/surewalk/RequesterRequestScreen.java
+++ app/src/main/java/ee461l/surewalk/RequesterRequestScreen.java
@@ -32,6 +32,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -121,7 +122,8 @@ public class RequesterRequestScreen extends FragmentActivity implements OnMapRea
                         v.setTag(0);
 
                         LatLng destLatLng = new LatLng(destAddress.getLatitude(), destAddress.getLongitude());
-                        destinationMarker = mMap.addMarker(new MarkerOptions().position(destLatLng).title("Destination"));
+                        destinationMarker = mMap.addMarker(new MarkerOptions().position(destLatLng).title("Destination")
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
                         //destinationLocData = destLatLng; // set destination location data to the destination information
 
                         //Send the request
