059eff730911b38f3e6e906a073cb88979d7a076
Jiwhan Son <jiwhanese@gmail.com>
2016-11-27 19:17:08 -0600

    Map for walking screen

--- app/src/main/java/ee461l/surewalk/WalkerCurrentlyWalkingScreen.java
+++ app/src/main/java/ee461l/surewalk/WalkerCurrentlyWalkingScreen.java
@@ -2,8 +2,11 @@ package ee461l.surewalk;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AlertDialog;
@@ -17,6 +20,12 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -34,7 +43,12 @@ import com.google.gson.Gson;
 import Users.Request;
 import Users.Requester;
 
-public class WalkerCurrentlyWalkingScreen extends FragmentActivity implements OnMapReadyCallback {
+public class WalkerCurrentlyWalkingScreen extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+    public static final String TAG = WalkerCurrentlyWalkingScreen.class.getSimpleName();
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
     private ImageView profilePicture;
     private TextView txtName;
     private Button btnCallRequester;
@@ -45,6 +59,8 @@ public class WalkerCurrentlyWalkingScreen extends FragmentActivity implements On
     private Users.Request currentRequest;
     private String requestKey;
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,9 +69,18 @@ public class WalkerCurrentlyWalkingScreen extends FragmentActivity implements On
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.WalkerCurrentlyWalkingMap);
         mapFragment.getMapAsync(this);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
 
         profilePicture = (ImageView) findViewById(R.id.RequesterPicture);
-        txtName = (TextView) findViewById(R.id.WalkerName);
+        //txtName = (TextView) findViewById(R.id.WalkerName);
         btnCallRequester = (Button) findViewById(R.id.CallRequester);
         btnMessageRequester = (Button) findViewById(R.id.TextRequeseter);
         btnCancelWalk = (Button) findViewById(R.id.CancelWalk);
@@ -63,7 +88,7 @@ public class WalkerCurrentlyWalkingScreen extends FragmentActivity implements On
         Bundle extras = getIntent().getExtras();
         currentRequest =  new Gson().fromJson(extras.getString("RequestInfo"), Users.Request.class);
 
-        txtName.setText("You are on your way to " + currentRequest.getRequester().username);
+        //txtName.setText("You are on your way to " + currentRequest.getRequester().username);
         requesterPhoneNumber = currentRequest.getRequester().phoneNumber;
         requestKey = extras.getString("RequestKey");
         FirebaseVariables.getDatabaseReference().child("Requests").addListenerForSingleValueEvent(
@@ -167,10 +192,90 @@ public class WalkerCurrentlyWalkingScreen extends FragmentActivity implements On
             return;
         }
         mMap.setMyLocationEnabled(true);
-        LatLng requester = new LatLng(currentRequest.getCurrentLatitude(), currentRequest.getCurrentLongitude());
+        LatLng requester1 = new LatLng(currentRequest.getCurrentLatitude(), currentRequest.getCurrentLongitude());
         LatLng destination = new LatLng(currentRequest.getDestinationLatitude(), currentRequest.getDestinationLongitude());
-        mMap.addMarker(new MarkerOptions().position(requester));
+        mMap.addMarker(new MarkerOptions().position(requester1));
         mMap.addMarker(new MarkerOptions().position(destination).title("Destination")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "Location services connected.");
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location == null) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+        else {
+            handleNewLocation(location);
+        }
+    }
+
+    private void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+        /*walkerLocation = location; // Set the current location data to the current location*/
+
+
+        // currentLocationMarker = mMap.addMarker(new MarkerOptions().position(latLng).title("Current Location")
+        //        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Location services suspended. Please reconnect.");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //setUpMapIfNeeded();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //currentLocationMarker.remove();
+        //handleNewLocation(location);
     }
 }
